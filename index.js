e19537376b8fdcc23f536e2681e046ef5529b03f
Mohammed Siddique <khanshahid9066@gmail.com>
2025-02-02 21:22:34 +0530

    black_jack_game
    Add to sum when newCard is clicked

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById('message-el')
+let sumEl = document.querySelector('#sum-el')
+let cardEl = document.querySelector('#cards-el')
 
-if (sum <= 20){
-    message = "Do you to draw a new card?";
-} else if (sum ===21) {
-    message = "Whohoo! You've got BlackJack!";
-    hasBlack = true
-} else {
-    message = "You're out of the game";
-    isAlive = false
+function startgame() {
+    
+    cardEl.textContent = "Cards: " + firstCard +" " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    
+
+    if (sum <= 20){
+        message = "Do you to draw a new card?";
+    } else if (sum ===21) {
+        message = "Whohoo! You've got BlackJack!";
+        hasBlack = true
+    } else {
+        message = "You're out of the game";
+        isAlive = false
+    }
+    messageEl.textContent = message
+
+    console.log(message)
 }
 
-console.log(message)
+function newCard() {
+    console.log("Drawing a new card")
+}
+
+
