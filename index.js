6c2c91d6ed76cb1829c7f98abde6ebc6a38c5263
Mohammed Siddique <khanshahid9066@gmail.com>
2025-02-08 23:23:56 +0530

    black_jack_game
    Push new card to array

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlack = false
 let isAlive = true
@@ -14,7 +15,7 @@ function startGame(){
 
 function renderGame() {
     
-    cardEl.textContent = "Cards: " + firstCard +" " + secondCard
+    cardEl.textContent = "Cards: " + cards[0] +" " + cards[1]
     sumEl.textContent = "Sum: " + sum
     
 
